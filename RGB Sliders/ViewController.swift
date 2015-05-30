caa8068c509c90ff001bf94208f920f825b733ac
Jeffrey <ji8521@gmail.com>
2015-05-30 14:23:58 -0400

    Added segue to second View Controller

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        redSlider.value = defaults.floatForKey("red")
+        greenSlider.value = defaults.floatForKey("green")
+        blueSlider.value = defaults.floatForKey("blue")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +36,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func updateBackgroundColor(sender: AnyObject) {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+  
+        let defaults = NSUserDefaults.standardUserDefaults()  //1
+        defaults.setFloat(redSlider.value, forKey: "red")   //2
+        defaults.setFloat(greenSlider.value, forKey: "green")
+        defaults.setFloat(blueSlider.value, forKey: "blue")
+        defaults.synchronize()    //3
+    
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "openColor") {
+            let newViewController = segue.destinationViewController as! UIViewController
+            newViewController.view.backgroundColor = colorSquare.backgroundColor
+        }
 }
-
+}
